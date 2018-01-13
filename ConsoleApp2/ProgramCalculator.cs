aafb4d76274bcf7820e2dadebb9077937a3679d2
Ratmir Mugattarov <ratmirmugattarov@gmail.com>
2018-01-13 14:10:57 +0700

    add exit answer control

--- ConsoleApp2/ProgramCalculator.cs
+++ ConsoleApp2/ProgramCalculator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Console_Calculator
 {
     class Program
@@ -11,23 +12,51 @@ namespace Console_Calculator
 
         static void Main()
         {
-            double number_one_double = 0;
+            double number_one_double = 0; 
             double number_two_double = 0;
             double result = 0;
             char operand = '0';
             bool repeat = true;
+            int input_control = 0;
             string instructions = "Welcome to calculator. Please enter what you need to calculate. Sample 2+3 and press Enter";
             string number_one_string;
             string number_two_string;
+            string list_of_allowed = "1234567890.+-*/"; // ? how do i need to use 'new' here?
 
+
+            // While repeat is true we go in sircles asking to continue
             while (repeat)
             {
                 Console.WriteLine(instructions);
                 string input = Console.ReadLine();
                 Console.SetCursorPosition(input.Length, Console.CursorTop - 1);
 
+                // Checking  if the inout string contains only allowed symbols
+                input_control = 0;
+                result = 0;
+                for (int index = 0; index <= input.Length-1; index++) 
+                {
+                    for (int jindex=0; jindex<= list_of_allowed.Length-1; jindex++)
+                    {
+                        if (input[index] == list_of_allowed[jindex])
+                        {
+                            input_control++;
+                        }
+                        
+                    }
+                }
+                if (input_control != input.Length)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Input Error. Please type carefully!");
+                    goto Again;
+                }
+
                 char char_index;
-                for (int index=0; index<=input.Length; index++)
+
+                // spil the string before and after operation sign and try to cast the parts to double.
+                // starting from the 1st symbol because of possibility of negative value
+                for (int index=1; index<=input.Length; index++)
                 {
                     char_index = input[index];
                     if (char_index == '+' || char_index == '-' || char_index == '*' || char_index == '/')
@@ -44,14 +73,17 @@ namespace Console_Calculator
                         catch (FormatException)
                         {
                             Console.WriteLine("Input string is not a sequence of digits");
+                            goto Again;
                         }
                         catch (OverflowException)
                         {
                             Console.WriteLine("The number cannot fit in double");
+                            goto Again;
                         }
                         catch (IndexOutOfRangeException)
                         {
                             Console.WriteLine("Input string is not a sequence of digits");
+                            goto Again;
                         }
 
                     }  
@@ -67,31 +99,43 @@ namespace Console_Calculator
                         break;
                     case '+':
                         result = number_one_double + number_two_double;
+                        Console.Write("=");
+                        Console.WriteLine(result);
                         break;
                     case '-':
                         result = number_one_double - number_two_double;
+                        Console.Write("=");
+                        Console.WriteLine(result);
                         break;
                     case '/':
                         result = number_one_double / number_two_double;
+                        Console.Write("=");
+                        Console.WriteLine(result);
                         break;
                     case '*':
                         result = number_one_double * number_two_double;
+                        Console.Write("=");
+                        Console.WriteLine(result);
                         break;
                     default:
                         throw new InvalidOperationException("Unknown item type");
                 }
 
-                Console.Write("=");
-                Console.WriteLine(result);
+                
+                Again:
                 Console.WriteLine("Go again? Y/N");
                 string go = Console.ReadLine();
                 if (go == "Y" || go == "y")
                 {
                     repeat = true;
                 }
-                else
+                else if (go == "N" || go == "n")
                 {
                     repeat = false;
+                }
+                else
+                {
+                    goto Again;
                 }
             }
 
